0046faa3e3ed062d3f986bba6bf19fda27759a3b
Chairshot215 <keefe.lehman@gmail.com>
2013-05-31 20:05:54 -0400

    Start cleaning up and categorizing “shootervm-vendor-blobs.mk”

--- shootervm/shootervm-vendor-blobs.mk
+++ shootervm/shootervm-vendor-blobs.mk
@@ -45,35 +45,52 @@ PRODUCT_COPY_FILES += \
     vendor/htc/shootervm/proprietary/dmagent:system/bin/dmagent \
     vendor/htc/shootervm/proprietary/wimaxDaemon:system/bin/wimaxDaemon
 
-# All the blobs necessary for shootervm
+# GPS 
+PRODUCT_COPY_FILES += \
+    vendor/htc/shootervm/proprietary/gps.shooter.so:/system/lib/hw/gps.shooter.so \
+    vendor/htc/shootervm/proprietary/gps.goldfish.so:/system/lib/hw/gps.goldfish.so 
+
+# Touchscreen 
 PRODUCT_COPY_FILES += \
     vendor/htc/shootervm/proprietary/keypress:/system/bin/keypress \
     vendor/htc/shootervm/proprietary/touchscreen:/system/bin/touchscreen \
-    vendor/htc/shootervm/proprietary/touchevent:/system/bin/touchevent \
-    vendor/htc/shootervm/proprietary/charging:/system/bin/charging \
-    vendor/htc/shootervm/proprietary/htcbatt:/system/bin/htcbatt \
+    vendor/htc/shootervm/proprietary/touchevent:/system/bin/touchevent 
+
+# Data / Ril 
+PRODUCT_COPY_FILES += \
+    vendor/htc/shootervm/proprietary/ipd:/system/bin/ipd \
     vendor/htc/shootervm/proprietary/load-modem.sh:/system/bin/load-modem.sh \
-    vendor/htc/shootervm/proprietary/mpdecision:/system/bin/mpdecision \
     vendor/htc/shootervm/proprietary/netmgrd:/system/bin/netmgrd \
     vendor/htc/shootervm/proprietary/qmuxd:/system/bin/qmuxd \
-    vendor/htc/shootervm/proprietary/thermald:system/bin/thermald \
     vendor/htc/shootervm/proprietary/wireless_modem:system/bin/wireless_modem \
     vendor/htc/shootervm/proprietary/wireless_modem:system/xbin/wireless_modem \
-    vendor/htc/shootervm/proprietary/libaudioalsa.so:/system/lib/libaudioalsa.so \
-    vendor/htc/shootervm/proprietary/libaudcal.so:/system/lib/libaudcal.so \
-    vendor/htc/shootervm/proprietary/libacdbmapper.so:system/lib/libacdbmapper.so \
-    vendor/htc/shootervm/proprietary/libacdbloader.so:system/lib/libacdbloader.so \
-    vendor/htc/shootervm/proprietary/libC2D2.so:/system/lib/libC2D2.so \
-    vendor/htc/shootervm/proprietary/libc2d2_z180.so:/system/lib/libc2d2_z180.so \
-    vendor/htc/shootervm/proprietary/libcamerapp.so:/system/lib/libcamerapp.so \
-    vendor/htc/shootervm/proprietary/libcameraservice.so:/system/lib/libcameraservice.so \
-    vendor/htc/shootervm/proprietary/libcameraSP.so:/system/lib/libcameraSP.so \
-    vendor/htc/shootervm/proprietary/libcameraface.so:/system/lib/libcameraface.so \
     vendor/htc/shootervm/proprietary/libdll.so:/system/lib/libdll.so \
     vendor/htc/shootervm/proprietary/libdiag.so:/system/lib/libdiag.so \
     vendor/htc/shootervm/proprietary/libidl.so:/system/lib/libidl.so \
     vendor/htc/shootervm/proprietary/libdsi_netctrl.so:/system/lib/libdsi_netctrl.so \
     vendor/htc/shootervm/proprietary/libdsutils.so:/system/lib/libdsutils.so \
+    vendor/htc/shootervm/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so \
+    vendor/htc/shootervm/proprietary/libhtc_ril_switch.so:/system/lib/libhtc_ril_switch.so \
+    vendor/htc/shootervm/proprietary/libnetmgr.so:/system/lib/libnetmgr.so \
+    vendor/htc/shootervm/proprietary/libqmi.so:/system/lib/libqmi.so \
+    vendor/htc/shootervm/proprietary/libqmiservices.so:/system/lib/libqmiservices.so \
+    vendor/htc/shootervm/proprietary/libqdp.so:/system/lib/libqdp.so 
+
+# Audio
+PRODUCT_COPY_FILES += \
+    vendor/htc/shootervm/proprietary/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \
+    vendor/htc/shootervm/proprietary/libaudioalsa.so:/system/lib/libaudioalsa.so \
+    vendor/htc/shootervm/proprietary/libaudcal.so:/system/lib/libaudcal.so \
+    vendor/htc/shootervm/proprietary/libacdbmapper.so:system/lib/libacdbmapper.so \
+    vendor/htc/shootervm/proprietary/libacdbloader.so:system/lib/libacdbloader.so \
+    vendor/htc/shootervm/proprietary/sound8x60:/system/bin/sound8x60 
+
+# GPU
+PRODUCT_COPY_FILES += \
+    vendor/htc/shootervm/proprietary/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
+    vendor/htc/shootervm/proprietary/libOpenVG.so:/system/lib/libOpenVG.so \
+    vendor/htc/shootervm/proprietary/libC2D2.so:/system/lib/libC2D2.so \
+    vendor/htc/shootervm/proprietary/libc2d2_z180.so:/system/lib/libc2d2_z180.so \
     vendor/htc/shootervm/proprietary/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
     vendor/htc/shootervm/proprietary/libgemini.so:/system/lib/libgemini.so \
     vendor/htc/shootervm/proprietary/libgemini2.so:/system/lib/libgemini2.so \
@@ -81,38 +98,45 @@ PRODUCT_COPY_FILES += \
     vendor/htc/shootervm/proprietary/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
     vendor/htc/shootervm/proprietary/libGLESv2S3D_adreno200.so:/system/lib/egl/libGLESv2S3D_adreno200.so \
     vendor/htc/shootervm/proprietary/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
-    vendor/htc/shootervm/proprietary/libgsl.so:/system/lib/libgsl.so \
-    vendor/htc/shootervm/proprietary/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
-    vendor/htc/shootervm/proprietary/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \
-    vendor/htc/shootervm/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so \
-    vendor/htc/shootervm/proprietary/libhtc_ril_switch.so:/system/lib/libhtc_ril_switch.so \
+    vendor/htc/shootervm/proprietary/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
+    vendor/htc/shootervm/proprietary/libgsl.so:/system/lib/libgsl.so 
+
+# Camera
+PRODUCT_COPY_FILES += \
+    vendor/htc/shootervm/proprietary/libcamerapp.so:/system/lib/libcamerapp.so \
+    vendor/htc/shootervm/proprietary/libcameraservice.so:/system/lib/libcameraservice.so \
+    vendor/htc/shootervm/proprietary/libcameraSP.so:/system/lib/libcameraSP.so \
+    vendor/htc/shootervm/proprietary/libcameraface.so:/system/lib/libcameraface.so \
+    vendor/htc/shootervm/proprietary/liboemcamera.so:/system/lib/liboemcamera.so \
+    vendor/htc/shootervm/proprietary/camera.default.so:/system/lib/hw/camera.default.so 
+
+# MM
+PRODUCT_COPY_FILES += \
     vendor/htc/shootervm/proprietary/libmmipl.so:/system/lib/libmmipl.so \
     vendor/htc/shootervm/proprietary/libmmmpo.so:/system/lib/libmmmpo.so \
     vendor/htc/shootervm/proprietary/libmmjpeg.so:/system/lib/libmmjpeg.so \
     vendor/htc/shootervm/proprietary/libmmjps.so:/system/lib/libmmjps.so \
     vendor/htc/shootervm/proprietary/libmmjpeg2.so:/system/lib/libmmjpeg2.so \
     vendor/htc/shootervm/proprietary/libmm-color-convertor.so:/system/lib/libmm-color-convertor.so \
-    vendor/htc/shootervm/proprietary/libmmstereo.so:/system/lib/libmmstereo.so \
+    vendor/htc/shootervm/proprietary/libmmstereo.so:/system/lib/libmmstereo.so 
+
+# Miscellaneous Blobs for ShooterVM
+PRODUCT_COPY_FILES += \
+    vendor/htc/shootervm/proprietary/hdmid:/system/bin/hdmid \
+    vendor/htc/shootervm/proprietary/fbtool:/system/bin/fbtool \
+    vendor/htc/shootervm/proprietary/charging:/system/bin/charging \
+    vendor/htc/shootervm/proprietary/htcbatt:/system/bin/htcbatt \
+    vendor/htc/shootervm/proprietary/mpdecision:/system/bin/mpdecision \
+    vendor/htc/shootervm/proprietary/thermald:system/bin/thermald \
     vendor/htc/shootervm/proprietary/libmpl.so:/system/lib/libmpl.so \
     vendor/htc/shootervm/proprietary/libmllite.so:/system/lib/libmllite.so \
     vendor/htc/shootervm/proprietary/libmlplatform.so:/system/lib/libmlplatform.so \
-    vendor/htc/shootervm/proprietary/libnetmgr.so:/system/lib/libnetmgr.so \
-    vendor/htc/shootervm/proprietary/liboemcamera.so:/system/lib/liboemcamera.so \
     vendor/htc/shootervm/proprietary/libOlaEngine.so:/system/lib/libOlaEngine.so \
-    vendor/htc/shootervm/proprietary/libOpenVG.so:/system/lib/libOpenVG.so \
     vendor/htc/shootervm/proprietary/libposteffect.so:/system/lib/libposteffect.so \
     vendor/htc/shootervm/proprietary/libqc-opt.so:/system/lib/libqc-opt.so \
-    vendor/htc/shootervm/proprietary/libqmi.so:/system/lib/libqmi.so \
-    vendor/htc/shootervm/proprietary/libqdp.so:/system/lib/libqdp.so \
-    vendor/htc/shootervm/proprietary/libqmiservices.so:/system/lib/libqmiservices.so \
-    vendor/htc/shootervm/proprietary/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
-    vendor/htc/shootervm/proprietary/camera.default.so:/system/lib/hw/camera.default.so \
     vendor/htc/shootervm/proprietary/sensors.shooter.so:/system/lib/hw/sensors.shooter.so \
-    vendor/htc/shootervm/proprietary/gps.shooter.so:/system/lib/hw/gps.shooter.so \
-    vendor/htc/shootervm/proprietary/gps.goldfish.so:/system/lib/hw/gps.goldfish.so \
     vendor/htc/shootervm/proprietary/logcat2:/system/bin/logcat2 \
     vendor/htc/shootervm/proprietary/rmt_storage:/system/bin/rmt_storage \
-    vendor/htc/shootervm/proprietary/sound8x60:/system/bin/sound8x60 \
     vendor/htc/shootervm/proprietary/3D_calibration:/system/bin/3D_calibration \
     vendor/htc/shootervm/proprietary/3D_calibration_main:/system/bin/3D_calibration_main
 
